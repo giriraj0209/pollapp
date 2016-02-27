c05def9ab9e7b2506e69285a5f29a8038a8d4238
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-02-27 14:14:45 -0600

    Added questions module and design for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :polls
+  
+  resources :polls do
+  	resources :questions
+  end #this is to have a path for question as polls/1/questions
   root 'polls#index'
 end
