7d315fcb35035b448099338033f87b49274ae7ae
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-02-29 21:04:26 -0600

    Added replies show page and jqery tabs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
+before_action :set_question, only: [:show, :edit, :update, :destroy]
 before_action :set_poll
 before_action :set_kind_questions
   # GET /questions
